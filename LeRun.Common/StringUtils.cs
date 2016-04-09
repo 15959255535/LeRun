7e572070fbf8591f02c6f73e086f9d98f918c126
15959255535 <254745408@QQ.COM>
2016-04-09 15:18:11 +0800

    架构提交
    架构提交

---
+++ LeRun.Common/StringUtils.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2016/4/4
+ * Time: 16:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace LeRun.Common
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ LeRun.Common/WebHelper.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2016/4/4
+ * Time: 16:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace LeRun.Common
+{
+	/// <summary>
+	/// Web常用类
+	/// </summary>
+	public class WebHelper
+	{
+		public WebHelper()
+		{
+		}
+	}
+}
